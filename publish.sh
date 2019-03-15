98609e71dfe31233f328337bad45dd03b88f31a1
董永强 <yqdong@37degree.com>
2019-03-15 18:19:59 +0800

    fdji

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ git commit -m $msg
 read -p "分支名称：" branch
 git push origin $branch
 
-echo "npm version <newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease"
+echo -e "\033[32m npm version <newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease \033[0m"
 read -p "版本号：" version
 npm version $version
 git add -A
