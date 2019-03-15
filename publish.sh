98d63269d4e853f59e367190248de52b3ccaac18
董永强 <yqdong@37degree.com>
2019-03-15 17:46:02 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 git add -A;
-read -p "提交内容:" msg
+read -p "提交内容：" msg
 git commit -m $msg
+git push origin
 
 echo "npm version <newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease"
 read -p "版本号：" version
 npm version $version
 
-read -p "发布版本标签（latest）" tag
+read -p "发布版本标签（latest）：" tag
 npm publish --tag $tag || latest
