aa69bb557d4b5f8bd94cdb8bb27b38b86c495697
董永强 <yqdong@37degree.com>
2019-03-15 17:50:58 +0800

    test

--- publish.sh
+++ publish.sh
@@ -3,11 +3,19 @@
 git add -A;
 read -p "提交内容：" msg
 git commit -m $msg
-git push origin
+
+read -p "分支名称：" branch
+git push origin $branch
 
 echo "npm version <newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease"
 read -p "版本号：" version
 npm version $version
 
 read -p "发布版本标签（latest）：" tag
-npm publish --tag $tag || latest
+if [$tag]
+then
+  npm publish
+else
+  npm publish --tag $tag
+fi
+cnpm sync npm-dyq-test
